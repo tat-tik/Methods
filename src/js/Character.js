a4193c105c41c77bfaff552440afb7504b9f2c03
Tanya <an5227952@gmail.com>
2025-05-02 21:40:06 +0300

    убрала else

--- src/js/Character.js
+++ src/js/Character.js
@@ -3,17 +3,14 @@ export default class Character {
     constructor(name, type) {
         if (name.length < 2 || name.length > 10) {
               throw new Error('Ошибка! Имя персонажа должно быть длиной от 2 до 10 символов!');
-          } else {
-              this.name = name;
-          }
-          if (!Character.types.includes(type)) {
+          } 
+        if (!Character.types.includes(type)) {
             throw new Error('Ошибка! Некорректный тип персонажа!');
-        } else {
-            this.type = type
-        } 
+        }        
         
-        
+       
           this.name = name;
+          this.type = type;
           this.health = 100;
           this.level = 1;
           
